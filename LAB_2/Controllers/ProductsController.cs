cb7111da062888e6e6407962c1da7818ba8a5fc0
Ajay Kumar <200434382@student.georgianc.on.ca>
2020-01-16 21:36:45 -0500

    First commit

---
+++ LAB_2/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public String Details(String id)
+        {
+            string message = "You selected product: " + id; 
+            return message;
+        }
+    }
+}
